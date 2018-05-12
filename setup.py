799d4ceab88a655d156353cb4ee12960c6025be3
Joway <joway.w@gmail.com>
2018-05-12 18:35:13 +0800

    merge config json with default value - v0.0.3 (#1)
    * merge config json with default value
    
    * v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flying',
-    version='0.0.2',
+    version='0.0.3',
     keywords=('release', 'cli', 'docker', 'git', 'npm'),
     description='Flying : cli tools for manage releases',
     license='MIT License',
--- src/flying/config.py
+++ src/flying/config.py
@@ -1,12 +1,33 @@
+FLYING_DEFAULT = {
+    'conditions': [],
+    'pre_release': [],
+    'git': {
+        'enable': False,
+        'remote': 'origin',
+        'release_branch': 'master',
+    },
+    'docker': {
+        'enable': False,
+    },
+    'npm': {
+        'enable': False,
+        'package_path': './package.json',
+    },
+    'pypi': {
+        'enable': False,
+        'build_cmd': 'python setup.py sdist',
+        'upload_cmd': 'twine upload dist/*',
+    },
+}
 FLYING_TEMPLATE = {
     'name': 'project_name',
     'version': '0.0.0',
     'version_prefix': 'v',
     'conditions': [
-        'git status | grep "nothing to commit, working tree clean"'
+        # 'git status | grep "nothing to commit, working tree clean"'
     ],
     'pre_release': [
-        'cat ./flying.json'
+        # 'cat ./flying.json'
     ],
     'git': {
         'enable': True,
